c4e6679315720a8c4fee6d00204d89be352a46e5
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2017-02-09 14:18:22 -0800

    Implement login using sessions

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,12 +11,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get home" do
-    get home_path
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-
   test "should get help" do
     get help_path
     assert_response :success
